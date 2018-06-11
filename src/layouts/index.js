f5d0eb9c6de858ba6e8c84da9a69626f0f1a158a
realexperts <39710569+realexperts@users.noreply.github.com>
2018-06-11 20:02:43 +0200

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="REAL EXPERTS NETWORK GmbH" />
     <Navbar />
     <div>{children()}</div>
     <Footer />
