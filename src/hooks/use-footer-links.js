c28ec16a491d9c06e4e95ce795ebe0c93f1e8264
Steffen Schröder <schroeder@devid.net>
2019-07-26 10:07:16 +0200

    Fix query

--- src/hooks/use-footer-links.js
+++ src/hooks/use-footer-links.js
@@ -4,9 +4,9 @@ export const useFooterLinks = () => {
   const {settings, mobileImage, desktopImage} = useStaticQuery(
     graphql`
       query {
-        settings: settingsJson(id: {eq: "menu-settings"}) {
+        settings: settingsJson {
           footerMenu {
-            menuTitle
+            menutitle
             links {
               link
               title
