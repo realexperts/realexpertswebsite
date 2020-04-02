c2dd993da88f135f0528c51bd75430cbfa8b3ab7
Steffen Schröder <schroeder@devid.net>
2020-04-02 15:36:29 +0200

    Add footer links query. Check if link is internal or external

--- src/hooks/use-footer-links.js
+++ src/hooks/use-footer-links.js
@@ -4,6 +4,15 @@ export const useFooterLinks = () => {
   const {settings, slugs, mobileImage, desktopImage} = useStaticQuery(
     graphql`
       query {
+        settings: settingsJson(id: {eq: "footer-links-settings"}) {
+          footerLinks {
+            title
+            links {
+              url
+              title
+            }
+          }
+        }
         mobileImage:file(relativePath: { eq: "logo_esf_sachsen_smartphone.png" }) {
           childImageSharp {
             fluid(maxWidth: 480, quality: 100) {
