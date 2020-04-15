2d391200a40141eaaf1031836cdf55509de7bf30
Jörn Schmidt <schmidt@devid.net>
2020-04-15 22:33:52 +0200

    Maybe fix IE11 problem

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,5 @@ export const onClientEntry = () => {
     // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
     if (typeof window.IntersectionObserver === `undefined`) {
         import(`intersection-observer`);
-        console.log(`# IntersectionObserver is polyfilled!`);
     }
 };
