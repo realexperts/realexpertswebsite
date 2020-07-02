764b73a0c4ab3bcd0662ab034cba91c6a8114fbc
adeveloper-wq <dajesch@gmail.com>
2020-07-02 12:24:28 +0200

    Erster Versuch mit Cookiebot

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,12 @@ const TemplateWrapper = ({children, noHeader}) => {
 
   return (
     <div className={noHeader ? "content no-header" : "content"}>
-      <Helmet title="Home | Gatsby + Netlify CMS"/>
+      <Helmet
+        title="Home | Gatsby + Netlify CMS"
+        script={[
+          {'id': 'Cookiebot', 'src': 'https://consent.cookiebot.com/uc.js', 'data-cbid': '178192ac-766f-4cab-a110-039ac99eaf64', 'data-blockingmode': 'auto', 'type': 'text/javascript'}
+        ]}
+      />
       <header id="header-main"><Navbar/></header>
       <main>
         {children}
@@ -28,13 +33,13 @@ const TemplateWrapper = ({children, noHeader}) => {
       }
       <FooterLinks/>
       <Footer/>
-      <CookieConsent
+     {/*  <CookieConsent
         buttonText="Verstanden"
         cookieName="reCoockieConsent"
         style={{background: '#40A6B9'}}
         buttonStyle={{background: '#FFF', color: '#0E3F93', fontSize: '13px',}}>
         Wir verwenden Cookies, um Ihnen einen optimalen Service zu bieten. Wenn Sie auf dieser Seite weitersurfen, stimmen Sie der Verwendung von Cookies zu. Mehr zu <Link to="/datenschutz">Datenschutz</Link>.
-      </CookieConsent>
+      </CookieConsent> */} 
     </div>
   )
 };
