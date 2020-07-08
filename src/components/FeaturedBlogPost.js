9866a15ca7e534ad0c35512121a8476a57c83a33
adeveloper-wq <dajesch@gmail.com>
2020-07-08 15:16:08 +0200

    test gatsby-images

--- src/components/FeaturedBlogPost.js
+++ src/components/FeaturedBlogPost.js
@@ -14,7 +14,7 @@ export default class FeaturedBlogPost extends React.Component {
         <div className="blog-post-image">
           {post.fields.image &&
           <Link to={post.fields.slug}>
-            <Img fluid={post.fields.image.childImageSharp.fluid}/>
+            <Img loading="eager" fluid={post.fields.image.childImageSharp.fluid}/>
           </Link>}
         </div>
         <div className="post-content">
